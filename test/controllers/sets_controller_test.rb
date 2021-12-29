a8f51625d5dafd1f1732debdcb4aae0f99119d79
Mike Wittenauer <mikewittenauer@me.com>
2021-12-29 13:06:00 -0500

    Create set index and show page and write some simple tests

--- test/controllers/sets_controller_test.rb
+++ test/controllers/sets_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class SetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @set = lego_sets(:volkswagen)
+  end
+
+  test "index should return success" do
+    get sets_url
+    assert_response :success
+  end 
+
+  test "show should return success" do
+    get set_url(@set)
+    assert_response :success
+  end
 end
