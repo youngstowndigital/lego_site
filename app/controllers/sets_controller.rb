f34d2679be06394de1f75def929a5c28bea9d749
Mike Wittenauer <mikewittenauer@me.com>
2021-12-29 19:10:33 -0500

    Basic form and validation

--- app/controllers/sets_controller.rb
+++ app/controllers/sets_controller.rb
@@ -6,4 +6,24 @@ class SetsController < ApplicationController
     def show
         @set = LegoSet.find(params[:id])
     end
+
+    def new
+        @set = LegoSet.new
+    end
+
+    def create
+        @set = LegoSet.new(lego_set_params)
+
+        if @set.save
+            redirect_to set_path @set
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def lego_set_params
+        params.require(:set).permit(:name, :description, :item_number)
+    end
 end
