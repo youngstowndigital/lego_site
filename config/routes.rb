5d0eb536e51385c2c80dd15eb4f321c4ec90d267
Mike Wittenauer <mikewittenauer@me.com>
2022-01-04 19:55:32 -0500

    edit set functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources :sets, only: [:index, :show, :new, :create]
+  resources :sets, only: [:index, :show, :new, :create, :edit, :update]
 end
